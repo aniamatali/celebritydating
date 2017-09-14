b83c26572f6f304b81bf7ae0c2c1ffd19c73a73e
Alvin Niamatali and Hansen Hansen <hansen@>
2017-09-14 11:43:45 -0700

    completed celeb site trying out updating a commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+      if (age < 30 && gender === "female") {
+        $("#male1").show();
+        $("#male2").hide();
+        $("#female1").hide();
+        $("#female2").hide();
+      } else if (age >= 30 && gender === "female") {
+        $("#male2").show();
+        $("#male1").hide();
+        $("#female1").hide();
+        $("#female2").hide();
+      } else if (age < 30 && gender === "male") {
+        $("#female1").show();
+        $("#male1").hide();
+        $("#male2").hide();
+        $("#female2").hide();
+      } else if (age >= 30 && gender === "male") {
+        $("#female2").show();
+        $("#male1").hide();
+        $("#female1").hide();
+        $("#male2").hide();
+      }
+
+      event.preventDefault();
+
+  });
+
+
+
+});
